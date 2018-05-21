3e2d833cc3c6575fe9dc07e20a45b44f50335d5d
Louis Hoang <bawsing@gmail.com>
2018-05-20 17:24:55 -0700

    Generate auth_token_issued_at before saving user
    In the past , these methods are customed in the gem but gem changed locally are not included in version control. Hence, when running on production, the change are not affected -> test failed.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   acts_as_token_authenticatable
 
+  before_save :ensure_auth_token_issued_at
+
   DEFAULT_AUTH_TOKEN_REFRESH_CYCLE = 2.weeks
 
   # Include default devise modules. Others available are:
@@ -9,4 +11,19 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
 
 
+  def auth_token_expired_at
+    auth_token_issued_at + 2.weeks
+  end
+
+  def auth_token_expired?
+    auth_token_expired_at <= DateTime.now
+  end
+
+  private
+
+  def ensure_auth_token_issued_at
+    if authentication_token.blank? || authentication_token_changed?
+      self.auth_token_issued_at = DateTime.now
+    end
+  end
 end
