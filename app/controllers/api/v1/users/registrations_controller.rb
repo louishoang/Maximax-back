30e98fcfccc711eafbc6af2f326f47882a3d8084
Louis Hoang <bawsing@gmail.com>
2018-06-07 21:42:14 -0700

    Migrate first name, last name on users table

--- app/controllers/api/v1/users/registrations_controller.rb
+++ app/controllers/api/v1/users/registrations_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Api::V1::Users::RegistrationsController < Devise::RegistrationsController
-  # before_action :configure_sign_up_params, only: [:create]
+  before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
 
   def create
@@ -60,9 +60,11 @@ class Api::V1::Users::RegistrationsController < Devise::RegistrationsController
   # protected
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_up_params
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
-  # end
+  def configure_sign_up_params
+    devise_parameter_sanitizer
+      .permit(:user,
+              keys: %i[first_name last_name email password password_confirmation])
+  end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_account_update_params
