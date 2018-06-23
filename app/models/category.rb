4f06ff608490f3465ab60765e0afa2633c67239d
Louis Hoang <bawsing@gmail.com>
2018-06-22 17:14:58 -0700

    Allow category Not to have a parent

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,7 +2,7 @@
 
 class Category < ApplicationRecord
   has_many :products
-  belongs_to :parent, class_name: 'Category', foreign_key: 'parent_id'
+  belongs_to :parent, class_name: 'Category', foreign_key: 'parent_id', optional: true
   has_many :children, class_name: 'Category', foreign_key: 'parent_id'
   validates :name, presence: true, length: { maximum: 255 }
 
